240744e1f769a56ddecb69cbb10af8353cb04c51
KlausChan0523 <xing930629@163.com>
2018-11-06 22:23:33 +0800

    版本合并

--- anissa/header.php
+++ anissa/header.php
@@ -10,25 +10,22 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-
-<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=no" />
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=no" />
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
 
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'anissa' ); ?></a>
-
 	<header id="masthead" class="site-header" role="banner">
 		<div id="menu-touch" class="menu-touch">
+
 			<div class="menu-toggle flex-hc-vc" aria-controls="primary-menu" aria-expanded="">
-			<i class="lalaksks lalaksks-ic-menu"></i>
+				<i class="lalaksks lalaksks-ic-menu"></i>
 			</div>
 			<div class="flex-hc-vc">
 				<?php echo get_bloginfo('name'); ?>
@@ -36,8 +33,9 @@
 			<div class="flex-hc-vc">
 				<i class="lalaksks lalaksks-ic-search"></i>
 			</div>
+			
 		</div>
-		<nav id="site-navigation" class="main-navigation" role="navigation">		   
+		<nav id="site-navigation" class="main-navigation hide" role="navigation">		   
 			<?php wp_nav_menu( array( 
 				'theme_location' => 'primary', 
 				'menu_id' => 'primary-menu' ,
