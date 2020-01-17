b2101c662340765be333d755bab122bedc1e5327
KlausChan0523 <xing930629@163.com>
2020-01-17 17:18:28 +0800

    重要更新，调整目录结构；

--- KlausLab/js/useless/effects.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-  console.log("yalasuo");
-  jQuery("#slider").owlCarousel({
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3],
-  });
-  
-});
--- KlausLab_2.0_alpha/woocommerce.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- *
- * @package KlausLab
- */
-
-get_header(); ?>
-<?php woocommerce_content(); ?>
-<?php get_footer(); ?>
