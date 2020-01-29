97a49c69287913df81a35f09e155a699a9860d80
KlausChan0523 <xing930629@163.com>
2020-01-29 14:22:39 +0800

    临时提交，准备主题工程化

--- template-parts/page-movie.php
+++ template-parts/page-movie.php
@@ -12,15 +12,9 @@ wp_enqueue_style( 'doubancss', get_template_directory_uri() . '/css/douban.css',
 
 <div id="primary" class="page-movie main-area w-1">
     <main id="main" class="main-content" role="main">        
-        <h1 style="text-align: center;">我看过的电影</h1>
+        <h1 class="flex-hc-vc">我看过的电影</h1>
         <div id="douban-movie-list" class="doubanboard-list">
         </div>
-        <el-row :gutter="20">
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        </el-row>
     </main>
     <!-- #main -->
 </div>
