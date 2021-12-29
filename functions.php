066c71672fa225c8bc8b4e3a5bb359d5328d23d2
KlausChan03 <KlausChan03@gmail.com>
2021-12-29 11:28:37 +0800

    chore[function.php]: 环境变量，全局变量由function-main.php转移到function.php，清理部分无用的方法，为进一步前后端分离铺垫

--- functions.php
+++ functions.php
@@ -1,4 +1,27 @@
+
 <?php 
+
+// 定义目录变量
+// @if NODE_ENV = 'prod'
+// define('FE_ENV', "Production");
+// @endif
+// @if NODE_ENV = 'dev'
+define('FE_ENV', "Development");
+// @endif
+
+if (FE_ENV !== "Development") {
+  define('KL_THEME_DIR', get_template_directory() . '/dist');
+  define('KL_THEME_URI', get_template_directory_uri() . '/dist');
+} else {
+  define('KL_THEME_DIR', get_template_directory() . '/src');
+  define('KL_THEME_URI', get_template_directory_uri() . '/src');
+}
+
+define('KL_DIR', get_template_directory() . '/inc');
+define('KL_URI', get_template_directory_uri() . '/inc');
+define('page_template_directory', 'part-page/');
+define('content_template_directory', 'part-template/');
+
 // 任何添加于主题目录functions文件夹内的php文件都被调用到这里
 define('functions', TEMPLATEPATH.'/part-function');
 IncludeAll( functions );
